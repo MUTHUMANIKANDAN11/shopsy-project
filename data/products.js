8d3a3f2de3a58077ffe558a80fb71c6dcac81592
Muthu <muthumanikandan11mk@gmail.com>
2024-09-10 15:40:00 +0530

    order summary renders from cart

--- data/products.js
+++ data/products.js
@@ -1,3 +1,15 @@
+export function matchingProductItem(productId){
+  let matchedItem;
+  
+  products.forEach((item) => {
+    if(item.id === productId){
+      matchedItem = item;
+    }
+  });
+
+  return matchedItem;
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
