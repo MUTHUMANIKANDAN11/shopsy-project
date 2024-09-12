ec9ac161b82b9b5aea5efc02deda8cbac87ead4c
Muthu <muthumanikandan11mk@gmail.com>
2024-09-12 13:43:04 +0530

    create deliveryOption data structure

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+    {
+        id: 1,
+        date: 1,
+        priceCents: 999
+    },
+    {
+        id: 2,
+        date: 3,
+        priceCents: 499
+    },
+    {
+        id: 3,
+        date: 7,
+        priceCents: 0
+    }
+]
