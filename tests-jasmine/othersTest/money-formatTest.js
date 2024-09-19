e8e585d1258920dc6e79f9276da687b8709c390c
Muthu <muthumanikandan11mk@gmail.com>
2024-09-19 23:31:39 +0530

    test: remove default Jasmine tests and add moneyFormat tests

---
+++ tests-jasmine/othersTest/money-formatTest.js
@@ -0,0 +1,15 @@
+import { moneyFormat } from "../../others/money-format.js";
+
+describe("Test Suite: Money format conversion", () => {
+    it("Convert cents to dollar", () => {
+        expect(moneyFormat(2024)).toEqual('20.24');
+    });
+
+    it("Conversion with zero", () => {
+        expect(moneyFormat(0)).toEqual('0.00');
+    });
+
+    it("Conversion with floating point value", () => {
+        expect(moneyFormat(2000.5)).toEqual('20.01');
+    });
+});
