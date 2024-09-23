5f28e20c81fb46bd593cda7dc28d0a3892b250a8
Muthu <muthumanikandan11mk@gmail.com>
2024-09-23 20:38:53 +0530

    test: add unit tests for addToCart method to handle adding existing products

--- tests-jasmine/dataTest/cartTest.js
+++ tests-jasmine/dataTest/cartTest.js
@@ -18,7 +18,26 @@ describe("Test Suite: Add a product to cart", () => {
         expect(cart[0].id).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
         expect(cart[0].quantity).toEqual(1);
     });
-    //it("Add to exist product", () => {
 
-    //});
+    it("Add to exist product", () => {
+        spyOn(localStorage, 'setItem');
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                deliveryOptionId : 1,
+                id : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity : 1
+            }]);
+        });
+
+        loadCartFromLocal();
+
+        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 1);
+        
+        expect(cart.length).toEqual(1);
+
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].id).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(cart[0].quantity).toEqual(2);
+    });
 });
