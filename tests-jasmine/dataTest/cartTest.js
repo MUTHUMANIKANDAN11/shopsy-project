edc9731820ed33438ae64c0b5270811ea7245948
Muthu <muthumanikandan11mk@gmail.com>
2024-09-25 15:46:36 +0530

    test: implement beforeEach hook in cartTest.js

--- tests-jasmine/dataTest/cartTest.js
+++ tests-jasmine/dataTest/cartTest.js
@@ -1,9 +1,10 @@
 import { addToCart, cart, loadCartFromLocal } from "../../data/cart.js";
 
 describe("Test Suite: Add a product to cart", () => {
-    it("Add a new product", () => {
+    beforeEach(() => {
         spyOn(localStorage, 'setItem');
-
+    });
+    it("Add a new product", () => {
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([]);
         });
@@ -17,11 +18,14 @@ describe("Test Suite: Add a product to cart", () => {
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
         expect(cart[0].id).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
         expect(cart[0].quantity).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+            deliveryOptionId:1,
+            id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity:1
+        }]));
     });
 
     it("Add to exist product", () => {
-        spyOn(localStorage, 'setItem');
-
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 deliveryOptionId : 1,
@@ -39,5 +43,10 @@ describe("Test Suite: Add a product to cart", () => {
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
         expect(cart[0].id).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
         expect(cart[0].quantity).toEqual(2);
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+            deliveryOptionId:1,
+            id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity:2
+        }]));
     });
 });
