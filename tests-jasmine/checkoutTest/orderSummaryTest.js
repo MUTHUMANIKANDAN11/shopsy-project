bafd5c8d792c77272cdc6d775cac93a5d098c62f
Muthu <muthumanikandan11mk@gmail.com>
2024-09-25 14:49:42 +0530

    test: add unit tests for renderOrderSummary method to handle product deletion

--- tests-jasmine/checkoutTest/orderSummaryTest.js
+++ tests-jasmine/checkoutTest/orderSummaryTest.js
@@ -37,6 +37,39 @@ describe("Test Suite: renderOrderSummary", () => {
     });
 
     it("Remove a cart", () => {
+        document.querySelector('.js-test-container').innerHTML = `
+            <div class="js-order-summary"></div>
+            <div class="js-payment-summary"></div>
+            <div class="js-return-to-home-link"></div>
+        `;
+
+        const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+        const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+
+        spyOn(localStorage, "setItem");
+        spyOn(localStorage, "getItem").and.callFake(() => {
+            return JSON.stringify([{
+                deliveryOptionId : 2,
+                id : productId1,
+                quantity : 2
+            },{
+                deliveryOptionId: 1,
+                id : productId2,
+                quantity : 1
+            }]);
+        });
+
+        loadCartFromLocal();
         
+        renderOrderSummary();
+
+        document.querySelector(`.js-delete-quantity-link-${productId1}`).click();
+
+        expect(document.querySelector(`.js-delete-quantity-link-${productId1}`)).toEqual(null);
+        expect(cart.length).toEqual(1);
+        expect(document.querySelector(`.js-delete-quantity-link-${productId2}`)).not.toEqual(null);
+        expect(cart[0].id).toEqual(productId2);
+
+        document.querySelector('.js-test-container').innerHTML = '';
     });
 });
