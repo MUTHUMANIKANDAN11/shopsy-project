e635b5db834e95cb77831f5563dc6e7143934c20
Muthumanikandan <muthumanikandan11mk@gmail.com>
2024-10-14 10:14:11 +0530

    refactor: replace callbacks with promises in all files

--- tests-jasmine/checkoutTest/orderSummaryTest.js
+++ tests-jasmine/checkoutTest/orderSummaryTest.js
@@ -7,7 +7,6 @@ describe("Test Suite: renderOrderSummary", () => {
         loadProductFromBackend(() => {
             done();
         });
-        done();
     });
 
     const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
