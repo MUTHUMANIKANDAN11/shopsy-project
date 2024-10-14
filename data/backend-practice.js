16850e715814d2ef8abcb33e3220e6c116159dc1
Muthumanikandan <muthumanikandan11mk@gmail.com>
2024-10-14 09:55:48 +0530

    practice: implemented examples using promises

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -1,8 +1,46 @@
+/*
 const xhr = new XMLHttpRequest();
 
 xhr.addEventListener('load', () => {
     console.log(xhr.response);
 });
 
+
 xhr.open("GET", "https://supersimplebackend.dev/documentation");
-xhr.send();
+xhr.send()
+*/
+
+
+function start(fun){
+    setTimeout(() => {
+        console.log('start');
+        fun();
+    }, 4000);
+}
+
+function wait(fun){
+    setTimeout(() => {
+        console.log('wait');
+        fun();
+    }, 2000);
+}
+
+function end(){
+    setTimeout(() => {
+        console.log('end');
+    }, 1000);
+}
+
+new Promise((resolve) => {
+    start(() => {
+        resolve();
+    });
+}).then(() => {
+    return new Promise((resolve) => {
+        wait(() => {
+            resolve();
+        });
+    });
+}).then(() => {
+    end();
+});
